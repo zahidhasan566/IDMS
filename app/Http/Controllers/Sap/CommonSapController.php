9449567445882ea0620bd9aed79775a74cf80a9f
Md Zahid Hasan <jahid1234.jh@gmail.com>
2024-08-29 17:29:45 +0600

    jh

--- app/Http/Controllers/Sap/CommonSapController.php
+++ app/Http/Controllers/Sap/CommonSapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Sap;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -74,13 +75,14 @@ class CommonSapController extends Controller
     }
     public function storeSapCustomer(Request $request)
     {
+        dd($request);
         try {
             $requestCustomers = $request->all();
             $customerCount = 0;
             foreach ($requestCustomers as $singleCustomer) {
-                if (!empty($singleProduct['ProductCode']) && !empty($singleProduct['ProductName']) && !empty($singleProduct['BrandCode'])
-                    && !empty($singleProduct['UnitPrice']) && !empty($singleProduct['VAT']) && !empty($singleProduct['MRP'])
-                    && !empty($singleProduct['Business']) && !empty($singleProduct['Active'])) {
+                if (!empty($singleCustomer['ProductCode']) && !empty($singleProduct['ProductName']) && !empty($singleProduct['BrandCode'])
+                    && !empty($singleCustomer['UnitPrice']) && !empty($singleProduct['VAT']) && !empty($singleProduct['MRP'])
+                    && !empty($singleCustomer['Business']) && !empty($singleProduct['Active'])) {
 
                     //Check Already Exist Or Not
                     $checkExisting =  Product::where('ProductCode',$singleProduct['ProductCode'])->first();
@@ -93,21 +95,50 @@ class CommonSapController extends Controller
                         ], 409);
                     }
                     else{
-                        $productCount +=1;
+                        $customerCount +=1;
                         DB::beginTransaction();
 
                         //Product Create
-                        $product = new Product();
-                        $product->ProductCode = $singleProduct['ProductCode'];
-                        $product->ProductName = $singleProduct['ProductName'];
-                        $product->PackSize = $singleProduct['PackSize'];
-                        $product->BrandCode = $singleProduct['BrandCode'];
-                        $product->UnitPrice = $singleProduct['UnitPrice'];
-                        $product->VAT = $singleProduct['VAT'];
-                        $product->MRP = $singleProduct['MRP'];
-                        $product->Business = $singleProduct['Business'];
-                        $product->Active = $singleProduct['Active'];
-                        $product->save();
+                        $customer = new Customer();
+                        $customer->CustomerCode     = $request->CustomerCode;
+                        $customer->CustomerName     = $request->CustomerName;
+                        $customer->Gender           = $request->Gender;
+                        $customer->Add1             = $request->Add1;
+                        $customer->Add2             = $request->Add2 ? $request->Add2 : '';
+                        $customer->DistrictCode     = $request->DistrictCode;
+                        $customer->ThanaCode        = $request->ThanaCode;
+                        $customer->Email            = $request->Email;
+                        $customer->ContactPerson    = $request->ContactPerson ? $request->ContactPerson : '';
+                        $customer->Phone            = '';
+                        $customer->Mobile           = $request->Mobile;
+                        $customer->ThanaCode        = $request->ThanaCode;
+                        $customer->NID              = $request->NID;
+                        $customer->Business         = $request->Business ? $request->Business : '';
+                        $customer->DepotCode        = $request->DepotCode ? $request->DepotCode : '';
+                        $customer->CustomerType     = $request->CustomerType;
+                        $customer->PaymentMode      = $request->PaymentMode;
+                        $customer->CreditDays       = '';
+                        $customer->CreditLimit      = '';
+                        $customer->Metro            = '';
+                        $customer->RouteCode        = '';
+                        $customer->TTYCode          = '';
+                        $customer->UnionCode        = '';
+                        $customer->HatBazarCode     = '';
+                        $customer->CreateDate      = '';
+                        $customer->EditDate         = '';
+                        $customer->DiscountStatus   = '';
+                        $customer->Active           = 'Y';
+                        $customer->Fifo             = 'Y';
+                        $customer->Message          = '';
+                        $customer->Category         = '';
+                        $customer->RegistrationName = '';
+                        $customer->RegistrationAdd1 = '';
+                        $customer->RegistrationAdd2 = '';
+                        $customer->FathersName      = '';
+                        $customer->DOB              = null;
+                        $customer->OwnerType        = '';
+                        $customer->SubBusinessCode  = '';
+                        $customer->save();
                         DB::commit();
                     }
                 } else {
@@ -123,7 +154,7 @@ class CommonSapController extends Controller
             return response()->json([
                 'status' => 'Success',
                 'message' => 'Product Added Successfully',
-                'ProductCount'=>$productCount
+                'ProductCount'=>$customerCount
             ], 200);
         }
         catch (\Exception $exception) {
--- routes/sap.php
+++ routes/sap.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CommonController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('sap-product-add', [\App\Http\Controllers\Sap\CommonSapController::class, 'storeSapProduct']);
-Route::post('sap-product-customer', [\App\Http\Controllers\Sap\CommonSapController::class, 'storeSapCustomer']);
+Route::post('sap-customer-add', [\App\Http\Controllers\Sap\CommonSapController::class, 'storeSapCustomer']);
 
 
 //Route::group(['middleware' => ['jwt:api']], function () {
