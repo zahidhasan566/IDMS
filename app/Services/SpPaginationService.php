1129b03f46de58273266aed6b012e9887c8d1317
shafa <eaeishfaq@gmail.com>
2024-10-06 11:47:43 +0600

    inq

--- app/Services/SpPaginationService.php
+++ app/Services/SpPaginationService.php
@@ -42,6 +42,25 @@ class SpPaginationService
         ]);
     }
 
+    public static function paginate3($sp,$take='',$offset='')
+    {
+        $dataSet = SpPaginationService::getPdoResult($sp);
+        $countData = count($dataSet[0]) > 0 ? $dataSet[1][0]['PageNo']:0;
+        $dataSet[] = count($dataSet[0]) > 0 ? [['CountData' => intval($countData)]]:[['CountData' => 0]];
+        if ($take !== '' && $offset !== '') {
+            $from = $offset + 1;
+            $to = ($offset + $take) <= intval($countData) ? $offset + $take : intval($countData);
+        } else {
+            $from = '';
+            $to = '';
+        }
+        return response()->json([
+            'data' => $dataSet,
+            'from' => $from,
+            'to' => $to
+        ]);
+    }
+
     public static function getOffset($page,$take)
     {
         return $page === 1 ? 0 : $take * ($page - 1);
